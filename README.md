baf4b5d7a44516b000004a16a197a3ba63236bcc
Garret Bright <garret@drinknewtopia.com>
2020-09-11 20:34:06 -0700

    added java and updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # PasswordGenerator
+
+Password Generator
+
+Description:
+
+I have created an application that can generate a random password containing lowercase, uppercase, number, and symbol values.  The application can can help secure user info.  This solves the user problem of creating a secure password on their own. 
+
+Easy prompt feautres allow the user to pick what special characters to include in password, whether it be a lowercase, uppercase, number, or symbol.
+
+Installation:
+
+The site can be accessed through github URL 
+
+Usage:
+
+App was made to create secure password for user
+
+Credits:
+
+I used this tutorial to help create some fuctions 
+
+https://www.youtube.com/watch?v=duNmhKgtcsI
+
+License
+
+Copyright (c) 2020 Garret Bright
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,6 +1,131 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+  
+  var result = "";
+  var rLower = "abcdefghijklmnopqrstuvwxyz"; 
+  var rUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var rNumber = "0123456789";
+  var symbols = "!@#$%^&*()=,.<>/'";
+  var posschar = rLower;
+
+
+  // prompt user for number of password chars
+  
+  var numChars = prompt("How long should the password be?");
+
+  // IF length is < 8 OR length is > 128
+  // alert password length must be from 8 to 128
+
+  if (numChars < 8 || numChars > 128) {
+    alert("Password length must be from 8 to 128.");
+  } else {
+  
+  var l_char = prompt("Enter 1 for lowercase");
+
+  var u_char = prompt("Enter 1 for uppercase");
+  
+  var n_char = prompt("Enter 1 for numbers");
+
+  var s_char = prompt("Enter 1 for symbols");
+
+  // return the result
+
+  // if l_char
+
+  if (l_char == 1) {
+    result = result +getRandomLower(rLower);
+    // this lone commented out becuase we default posschar to rLower
+    // posschar = posschar + rLower;
+  } 
+
+  if (u_char == 1) {
+    result = result +getRandomUpper(rUpper);
+    posschar = posschar + rUpper;
+  } 
+
+  if (n_char == 1) {
+    result = result +getRandomNumber(rNumber);
+    posschar = posschar + rNumber; 
+  } 
+
+  if (s_char == 1) {
+    result = result +getRandomSymbol(symbols);
+    posschar = posschar + symbols;
+  } 
+
+
+  
+  for (; result.length< numChars;) {
+      result = result + getRandomNext(posschar);
+      
+    }
+  }
+  return result;
+}
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+  
+// function generatePassword() {
+//   var result = "";  
+    
+//     var specialChar = prompt("Does the password include a lowercase");
+  
+     
+//   // iF result is not true
+//   // alert password must be lowercase
+  
+//       if (specialChar === no) {
+//     alert ("Password must be lowercase");
+//   }   else {
+  
+//     // return the result
+//   result;
+// }
+//   }
+
+function getRandomNext(x) {
+  return x[Math.floor(Math.random() * x.length)]; 
+  
+}
+
+
+// // function for random lowercase letters
+  function getRandomLower(rLower){
+    // var rLower = "abcdefghijklmnopqrstuvwxyz" 
+    return rLower[Math.floor(Math.random() * rLower.length)]; 
+  }
+  
+// // function for random uppercase letters
+  function getRandomUpper(rUpper){
+  // var rUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+  return rUpper[Math.floor(Math.random() * rUpper.length)];
+}
+
+// // function for random numbers
+function getRandomNumber(rNumber){
+  // var rNumber = "0123456789" 
+  return rNumber[Math.floor(Math.random() * rNumber.length)];
+}
+
+
+// // function for random symbols
+function getRandomSymbol(symbols){
+  // var symbols = "!@#$%^&*()=,.<>/'"
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+// console.log(getRandomNumber()); 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +137,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
